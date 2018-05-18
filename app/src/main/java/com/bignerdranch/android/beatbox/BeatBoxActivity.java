344ecdd4b1f9def99a09de467aee722a9ca77e04
huyuqi <yuqi.hU@gmail.com>
2018-05-18 17:42:25 +0800

    新建的项目

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxActivity.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxActivity.java
@@ -1,13 +1,9 @@
 package com.bignerdranch.android.beatbox;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class BeatBoxActivity extends AppCompatActivity {
-
+public class BeatBoxActivity extends SingleFragmentActivity{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_beat_box);
+    protected BeatBoxFragment createFragment() {
+        return BeatBoxFragment.newInstance();
     }
+
 }
