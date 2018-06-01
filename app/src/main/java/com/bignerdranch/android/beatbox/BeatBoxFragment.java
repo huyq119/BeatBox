b51b9c6f4b5038362ac20376d098326d3a7c4328
huyuqi <yuqi.hU@gmail.com>
2018-06-01 16:59:06 +0800

    新建的项目

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -1,28 +1,43 @@
 package com.bignerdranch.android.beatbox;
 
-import android.app.Fragment;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bignerdranch.android.beatbox.BeatBox;
 import com.bignerdranch.android.beatbox.databinding.FragmentBeatBoxBinding;
 import com.bignerdranch.android.beatbox.databinding.ListItemSoundBinding;
 
+import java.util.List;
+
+
 public class BeatBoxFragment extends Fragment {
-    public static BeatBoxFragment newInstance(){
+
+    private BeatBox mBeatBox;
+
+    public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBeatBox = new BeatBox(getActivity());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_beat_box, container, false);
         binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
-        binding.recyclerView.setAdapter(new SoundAdapter());
+        binding.recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
         return binding.getRoot();
     }
     private class SoundHolder extends RecyclerView.ViewHolder{
@@ -34,6 +49,13 @@ public class BeatBoxFragment extends Fragment {
         }
     }
     private class SoundAdapter extends RecyclerView.Adapter<SoundHolder>{
+
+        private List<Sound> mSounds;
+
+        public SoundAdapter(List<Sound> sounds) {
+            mSounds = sounds;
+        }
+
         @NonNull
         @Override
         public SoundHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -49,7 +71,7 @@ public class BeatBoxFragment extends Fragment {
 
         @Override
         public int getItemCount() {
-            return 0;
+            return mSounds.size();
         }
     }
 }
